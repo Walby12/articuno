57f926e50d36ba05ed19e689f778f0ca19f62c23
Walby12 <alby09.bonf@gmail.com>
2025-04-13 22:28:54 +0200

    Added more ops

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ static STACK_CAPACITY: usize = 1024;
 enum Trap {
     StackOverFlow,
     StackUnderFlow,
+    DivByZero,
     Ok,
 }
 
@@ -16,6 +17,9 @@ fn trap_as_str(trap: Trap) -> String {
         Trap::StackOverFlow => {
             return String::from("Stack overflow");
         },
+        Trap::DivByZero => {
+            return String::from("Division by zero");
+        }
         Trap::Ok => {
             return String::from("Ok");
         }
@@ -32,6 +36,9 @@ struct Articuno {
 enum InstType {
     InstPush,
     InstPlus,
+    InstMinus,
+    InstMult,
+    InstDiv,
     InstDump,
 }
 
@@ -51,6 +58,30 @@ fn inst_push(operand: Word) -> Inst {
 fn inst_plus() -> Inst {
     let inst = Inst {
         ins_t: InstType::InstPlus,
+        operand: 0,
+    };
+    return inst;
+}
+
+fn inst_minus() -> Inst {
+    let inst = Inst {
+        ins_t: InstType::InstMinus,
+        operand: 0,
+    };
+    return inst;
+}
+
+fn inst_mul() -> Inst {
+    let inst = Inst {
+        ins_t: InstType::InstMult,
+        operand: 0,
+    };
+    return inst;
+}
+
+fn inst_div() -> Inst {
+    let inst = Inst {
+        ins_t: InstType::InstDiv,
         operand: 0,
     };
     return inst;
@@ -76,6 +107,36 @@ fn art_exec_inst(art: &mut Articuno,  inst: Inst) -> Trap {
             art.stack_size -= 1;
             
         },
+        InstType::InstMinus => {
+            if art.stack_size < 2 {
+                return Trap::StackUnderFlow;
+            }
+            let a = art.stack.pop().unwrap();
+            let b = art.stack.pop().unwrap();
+            art.stack.push(b - a);
+            art.stack_size -= 1;
+        },
+        InstType::InstMult => {
+            if art.stack_size < 2 {
+                return Trap::StackUnderFlow;
+            }
+            let a = art.stack.pop().unwrap();
+            let b = art.stack.pop().unwrap();
+            art.stack.push(a * b);
+            art.stack_size -= 1;
+        },
+        InstType::InstDiv => {
+            if art.stack_size < 2 {
+                return Trap::StackUnderFlow;
+            }
+            let a = art.stack.pop().unwrap();
+            if a == 0 {
+                return Trap::DivByZero;
+            }
+            let b = art.stack.pop().unwrap();
+            art.stack.push(b / a);
+            art.stack_size -= 1;
+        }
         InstType::InstPush => {
             if art.stack_size >= STACK_CAPACITY {
                 return Trap::StackOverFlow;
@@ -106,7 +167,7 @@ fn main() {
         stack: Vec::new(),
         stack_size: 0,
     };
-    let program = vec![inst_push(69), inst_push(5), inst_plus(), inst_dump()];
+    let program = vec![inst_push(69), inst_push(5), inst_plus(), inst_push(6), inst_div(), inst_dump()];
     
     for i in program {
         let trap: Trap = art_exec_inst(&mut art, i);
